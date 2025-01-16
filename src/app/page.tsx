09156099ec6d552013ba735543624074142a7262
Philip Huynh <wuflerz@outlook.com>
2025-01-16 10:33:05 +0200

    Hide login when session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
 import Link from 'next/link'
 import Account from '@/components/Account'
 import { Button } from '@/components/ui/button'
 
 export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -51,9 +57,11 @@ export default async function Home() {
             Read our docs
           </a>
         </div>
-        <Button asChild>
-          <Link href="/login">Login</Link>
-        </Button>
+        {!session && (
+          <Button asChild>
+            <Link href="/login">Login</Link>
+          </Button>
+        )}
         <Account />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
