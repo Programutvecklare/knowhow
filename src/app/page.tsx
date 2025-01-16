5fe4df5f5738506707060396e59de3952d7d16c6
Philip Huynh <wuflerz@outlook.com>
2025-01-16 10:31:38 +0200

    Initial login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import SignIn from '@/components/Sign-In';
-import Image from 'next/image';
-import { auth } from '@/lib/auth';
-import { headers } from 'next/headers';
+import Image from 'next/image'
+import Link from 'next/link'
+import Account from '@/components/Account'
+import { Button } from '@/components/ui/button'
 
 export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -55,8 +51,10 @@ export default async function Home() {
             Read our docs
           </a>
         </div>
-        <SignIn />
-        <p>{session?.user?.email}</p>
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+        <Account />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
@@ -106,5 +104,5 @@ export default async function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
--- src/components/Sign-In.tsx
+++ src/components/Sign-In.tsx
@@ -1,19 +1,29 @@
 'use client'
-
-import { authClient } from '@/lib/auth-client'
-import { Button } from './ui/button'
 import Github from './ui/github'
+import { authClient } from '@/lib/auth-client'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
-export default function SignIn() {
-	const signIn = async () => {
-		await authClient.signIn.social({
-			provider: 'github',
-		})
-	}
+export default function LoginPage() {
+  const signIn = async () => {
+    await authClient.signIn.social({
+      provider: 'github',
+      callbackURL: '/',
+    })
+  }
 
-	return (
-		<Button onClick={signIn}>
-			<Github /> Login with GitHub
-		</Button>
-	)
+  return (
+    <div className="flex items-center justify-center min-h-dvh">
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign in to Knowhow</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button className="w-full" onClick={signIn}>
+            <Github /> Login with GitHub
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
