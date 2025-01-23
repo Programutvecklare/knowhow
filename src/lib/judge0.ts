ce475200795072580d91bee4419abbabec51831a
zekken <blch.namt@gmail.com>
2025-01-23 13:48:27 +0200

    url as env variable

--- src/lib/judge0.ts
+++ src/lib/judge0.ts
@@ -1,17 +1,15 @@
 export default async function judge0(code: string) {
-  const judge0Response = await fetch(
-    'http://37.27.6.76:2358/submissions?wait=true',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        language_id: 63,
-        source_code: code,
-      }),
-    }
-  )
-  console.log(judge0Response)
-  return judge0Response
+  const response = await fetch(process.env.JUDGE0_URL as string, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      language_id: 63,
+      source_code: code,
+    }),
+  })
+
+  const data = response.json()
+  return data
 }
