356b51139f2342631a1ce5f0ad3ddbc5e5b0f8dc
Philip Huynh <wuflerz@outlook.com>
2025-01-23 13:31:45 +0200

    Fix height and some cleanup

--- src/components/Challenges.tsx
+++ src/components/Challenges.tsx
@@ -10,7 +10,7 @@ import {
 import Link from 'next/link'
 import { Separator } from '@/components/ui/separator'
 import getLevelDescription from '@/lib/level'
-import { Button } from './ui/button'
+import { Button } from '@/components/ui/button'
 
 export default function Challenges({
   challenges,
@@ -18,7 +18,7 @@ export default function Challenges({
   challenges: Challenge[]
 }) {
   return (
-    <div className="p-4">
+    <div className="container mx-auto p-4">
       <div className="flex items-center justify-between">
         <h1 className="text-xl">Challenges</h1>
         <Button asChild>
@@ -28,45 +28,59 @@ export default function Challenges({
 
       <div className="p-6 mt-4 border-[1px] rounded-md border-[E2E8F0]">
         <Table>
-          <TableHeader className="w-full">
-            <TableRow className="w-full">
-              <TableHead className="w-[40%]">Title</TableHead>
-              <TableHead className="w-[40%]">Description</TableHead>
-              <TableHead className="w-[40%]">Level</TableHead>
-              <TableHead className="w-[40%]">Created</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {challenges.map((challenge: Challenge) => {
-              const createdAtResult = formatDistanceToNow(
-                new Date(challenge.createdAt),
-                {
-                  addSuffix: true,
-                }
-              )
+          {challenges.length ? (
+            <>
+              <TableHeader className="w-full">
+                <TableRow className="w-full">
+                  <TableHead className="w-[40%]">Title</TableHead>
+                  <TableHead className="w-[40%]">Description</TableHead>
+                  <TableHead className="w-[40%]">Level</TableHead>
+                  <TableHead className="w-[40%]">Created</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {challenges.map((challenge: Challenge) => {
+                  const createdAtResult = formatDistanceToNow(
+                    new Date(challenge.createdAt),
+                    {
+                      addSuffix: true,
+                    }
+                  )
 
-              const levelDescription = getLevelDescription(challenge.level)
+                  const levelDescription = getLevelDescription(challenge.level)
 
-              return (
-                <Link
-                  href={`/challenges/${challenge.id}`}
-                  key={challenge.id}
-                  legacyBehavior
-                >
-                  <TableRow className="cursor-pointer">
-                    <TableCell className="w-[40%]">{challenge.title}</TableCell>
-                    <TableCell className="w-[40%]">
-                      {challenge.description}
-                    </TableCell>
-                    <TableCell className="w-[40%]">
-                      {levelDescription}
-                    </TableCell>
-                    <TableCell className="w-[40%]">{createdAtResult}</TableCell>
-                  </TableRow>
-                </Link>
-              )
-            })}
-          </TableBody>
+                  return (
+                    <Link
+                      href={`/challenges/${challenge.id}`}
+                      key={challenge.id}
+                      legacyBehavior
+                    >
+                      <TableRow className="cursor-pointer">
+                        <TableCell className="w-[40%]">
+                          {challenge.title}
+                        </TableCell>
+                        <TableCell className="w-[40%]">
+                          {challenge.description}
+                        </TableCell>
+                        <TableCell className="w-[40%]">
+                          {levelDescription}
+                        </TableCell>
+                        <TableCell className="w-[40%]">
+                          {createdAtResult}
+                        </TableCell>
+                      </TableRow>
+                    </Link>
+                  )
+                })}
+              </TableBody>
+            </>
+          ) : (
+            <TableBody>
+              <TableRow>
+                <TableCell>No challenges available.</TableCell>
+              </TableRow>
+            </TableBody>
+          )}
         </Table>
         <Separator />
       </div>
